c1263284c256e966b903c9e024b3157e687b4ce9
EDMR <emermontes15@gmail.com>
2023-07-18 17:22:30 -0500

    Mensaje de prueba

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,31 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
 
-function DecimalABinario(num) {}
+  var suma = 0;
+  var exponente = num.length-1;
+    
+  for (let i = 0; i < num.length; i++) {
+    suma = suma + num[i]*Math.pow(2,exponente)
+    exponente--;
+    
+    }
+ return suma;
+}
+
+function DecimalABinario(num) {
+
+   var binario = "";
+
+    while (num > 0) {
+      var residuo=num%2;
+      binario= residuo+binario;
+      num = Math.floor(num/2);
+    }
+
+   return binario;
+
+}
 
 module.exports = {
    BinarioADecimal,
